6e12ce1fe0267bc494177be517f82417309abe2a
Mari Lee <marilee.vln@gmail.com>
2025-11-13 12:34:24 -0500

    installed vitest

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node',
-    include: ['**/*.{test,spec}.ts'],
+    env: {
+      // this env variable ensures that the tests never alter our "real" database, only our test database
+      NODE_ENV: 'test',
+      // providing a different port for the server during testing ensures that the tests remain reliable even if the server is currently running in development
+      PORT: '3001',
+    },
+    setupFiles: './__tests__/setup.ts',
+    // set the environment to jsdom here
+    environment: 'jsdom',
   },
 });
